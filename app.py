1c4e425827858385299e85f1adaa983feb51a3a1
RuchiChopra43 <Ruchidubey13@gmail.com>
2020-09-28 09:34:11 +0100

    changes

--- app.py
+++ app.py
@@ -1,49 +1,54 @@
-# Importing the Libraries
 import logging
 import os
-# import pickle
 import sys
 import urllib
-
 import flask
 from flask import Flask, request, render_template
 from flask_cors import CORS
-from textblob import TextBlob
 
-def get_text_sentiment(text):
+def get_text_sentiment(review):
     '''
-    Utility function to classify sentiment of passed text
-    using textblob's sentiment method
+    CNN Model
     '''
-    # create TextBlob object of passed app text
-    text = text.replace("+", " ")
-    analysis = TextBlob(text)
+    from keras.datasets import imdb
+    from tensorflow.keras import models
+    from keras.preprocessing.text import text_to_word_sequence
+    from keras.preprocessing import sequence
 
-    # set sentiment
-    if not analysis.sentiment.polarity < 0:
-        return 'POSITIVE'
-    # elif analysis.sentiment.polarity == 0:
-    #     return 'MIXED'
-    else:
-        return 'NEGATIVE'
+    # A dictionary mapping words to an integer index
+    word_index = imdb.get_word_index()
+    # The first indices are reserved
+    word_index = {k: (v + 3) for k, v in word_index.items()}
+    word_index["<PAD>"] = 0
+    word_index["<START>"] = 1
+    word_index["<UNK>"] = 2  # unknown
+    word_index["<UNUSED>"] = 3
+    # reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+    # number of words per review
+    max_review_length = 500
+    b = models.load_model('model_cnn.h5')
+
+    # review = 'After 30 min I still did not know what the movie is about'
+    words = set(text_to_word_sequence(review))
+    words = [word_index[w] for w in words]
+    words = sequence.pad_sequences([words], maxlen=max_review_length)
+    proba = b.predict(words)
+    sentiment = 'POSITIVE' if proba * 100 > 0.35 else 'NEGATIVE'
+    return sentiment
 
 
 # Loading Flask and assigning the model variable
 app = Flask(__name__)
 CORS(app)
 app = flask.Flask(__name__, template_folder='templates')
-
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
-
-# with open('pickle_model_senti.pkl', 'rb') as handle:
-#     model = pickle.load(handle)
 
 
 @app.route('/')
 def main():
     return render_template('main.html')
-
 
 # Receiving the input url from the user and using Web Scrapping to extract the news content
 @app.route('/predict', methods=['GET', 'POST'])
@@ -52,30 +57,10 @@ def predict():
     text = urllib.parse.unquote(url)
     outcome = get_text_sentiment(text)
 
-    # LOAD SAVED PICKLE MODEL AND TEST IT WITH SOME RANDOM TEXT
-    # from sklearn.feature_extraction.text import CountVectorizer
-    # from keras.preprocessing import sequence
-    #
-    # # mdl = pickle.load(open('pickle_model_senti.pkl', 'rb'))
-    # cv = CountVectorizer()
-    # text = [text]
-    # text_vect = cv.fit_transform(text).toarray()
-    #
-    # max_words = 500
-    # text_vect = sequence.pad_sequences(text_vect, maxlen=max_words)
-    # pred = model.predict(text_vect)
-    # outcome = "Positive"
-    # if pred == 0:
-    #     outcome = "Positive"
-    # else:
-    #     outcome = "Negative"
-
-    # Passing the news article to the model and returing whether it is Fake or Real
-    # pred = model.predict([news])
-    # return render_template('main.html', prediction_text='The news is "{}"'.format(outcome))
     return render_template('main.html', prediction_text='It is the ' + outcome + ' sentiment')
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(port=port, debug=True, use_reloader=False)
+
+
